082dd73d0bfb736f04dc6d277d3ae4ef780f8d64
takumihara <takumihara1226@gmail.com>
2022-02-11 18:34:00 +0900

    update: no space between words

--- transcript.py
+++ transcript.py
@@ -17,7 +17,7 @@ def main():
 					speaker_start_times[item['start_time']] =item['speaker_label']
 			items = data['results']['items']
 			lines=[]
-			line=''
+			line=[]
 			time=0
 			speaker='null'
 			i=0
@@ -27,16 +27,16 @@ def main():
 				if item.get('start_time'):
 					current_speaker=speaker_start_times[item['start_time']]
 				elif item['type'] == 'punctuation':
-					line = line+content
+					line.append(content)
 				if current_speaker != speaker:
 					if speaker:
-						lines.append({'speaker':speaker, 'line':line, 'time':time})
-					line=content
+						lines.append({'speaker':speaker, 'line':''.join(line), 'time':time})
+					line=[content]
 					speaker=current_speaker
 					time=item['start_time']
 				elif item['type'] != 'punctuation':
-					line = line + ' ' + content
-			lines.append({'speaker':speaker, 'line':line,'time':time})
+					line.append(content)
+			lines.append({'speaker':speaker, 'line':''.join(line),'time':time})
 			sorted_lines = sorted(lines,key=lambda k: float(k['time']))
 			for line_data in sorted_lines:
 				line='[' + str(datetime.timedelta(seconds=int(round(float(line_data['time']))))) + '] ' + line_data.get('speaker') + ': ' + line_data.get('line')
